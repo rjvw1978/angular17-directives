6d1d6e3010b955875d1ea1ccbd24665f98680f03
Rojas Córsico Ivana Soledad <irojas@gmail.com>
2024-04-24 13:21:20 -0300

    ngClass directive example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { NgClass } from '@angular/common';
+import { on } from 'events';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [NgClass], 
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-directives';
+ image:string="assets/pic_bulboff.gif";
+ onTurn:boolean=false;
+ onTurnText="Encender"
+ turn()
+ {
+    this.onTurn=!this.onTurn;
+    if (this.onTurn){
+      this.image="assets/pic_bulbon.gif"
+      this.onTurnText="Apagar"
+    }
+    else{
+      this.image="assets/pic_bulboff.gif"
+      this.onTurnText="Encender"
+    }
+ }
+
+ 
 }
